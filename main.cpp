dd9c690e288a3eaff69856999c9c33ddea09ba97
AlexeyGreetsov <alexey.gretsov@gmail.com>
2025-08-18 22:46:11 +0300

    Format code

--- main.cpp
+++ main.cpp
@@ -7,18 +7,17 @@
 int main(int argc, char *argv[]) {
   std::cout << "Hello, World!\n";
 
-
   std::string m_dbhost = "localhost";
-  int         m_dbport = 5432;
+  int m_dbport = 5432;
   std::string m_dbname = "otus_messendger";
   std::string m_dbuser = "postgres";
   std::string m_dbpass = "postgres";
 
-  PGconn* dbConn = PQsetdbLogin(m_dbhost.c_str(), std::to_string(m_dbport).c_str(), 
-      nullptr, nullptr, m_dbname.c_str(), m_dbuser.c_str(), m_dbpass.c_str());
+  PGconn *dbConn = PQsetdbLogin(
+      m_dbhost.c_str(), std::to_string(m_dbport).c_str(), nullptr, nullptr,
+      m_dbname.c_str(), m_dbuser.c_str(), m_dbpass.c_str());
 
-  if (PQstatus(dbConn) != CONNECTION_OK && PQsetnonblocking(dbConn, 1) != 0 )
-  {
+  if (PQstatus(dbConn) != CONNECTION_OK && PQsetnonblocking(dbConn, 1) != 0) {
     throw std::runtime_error(PQerrorMessage(dbConn));
   }
 
@@ -26,23 +25,20 @@ int main(int argc, char *argv[]) {
 
   PQsendQuery(dbConn, query.c_str());
 
-  while (auto res_ = PQgetResult(dbConn)) 
-  {
-    if (PQresultStatus(res_) == PGRES_TUPLES_OK) 
-    {
+  while (auto res_ = PQgetResult(dbConn)) {
+    if (PQresultStatus(res_) == PGRES_TUPLES_OK) {
       int numRows = PQntuples(res_);
       int numCols = PQnfields(res_);
 
-      for (int i = 0; i < numRows; i++)
-      {
-        auto name = PQgetvalue (res_ ,i, 2);
-        auto ID = PQgetvalue (res_ , i, 1);
-        std::cout << "User " << name << " ID " << ID <<std::endl;
+      for (int i = 0; i < numRows; i++) {
+        auto name = PQgetvalue(res_, i, 2);
+        auto ID = PQgetvalue(res_, i, 1);
+        std::cout << "User " << name << " ID " << ID << std::endl;
       }
     }
 
-    if (PQresultStatus(res_) == PGRES_FATAL_ERROR){
-        std::cout<< PQresultErrorMessage(res_)<<std::endl;
+    if (PQresultStatus(res_) == PGRES_FATAL_ERROR) {
+      std::cout << PQresultErrorMessage(res_) << std::endl;
     }
 
     PQclear(res_);
